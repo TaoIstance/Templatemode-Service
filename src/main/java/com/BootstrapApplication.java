434e4c5c175c19298c2ad5e980354d51d4da2617
hetao <1293756386@qq.com>
2020-10-22 17:10:40 +0800

    first commit

---
+++ src/main/java/com/BootstrapApplication.java
@@ -0,0 +1,18 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * <p>Application TODO</p>
+ *
+ * @author hetao
+ * @date 2020/10/22 17:05
+ **/
+
+@SpringBootApplication
+public class BootstrapApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BootstrapApplication.class, args);
+    }
+}
---
+++ src/main/java/com/controller/TemplatemodeController.java
@@ -0,0 +1,11 @@
+package com.controller;
+
+/**
+ * <p>TemplatemodeController TODO</p>
+ *
+ * @author hetao
+ * @date 2020/10/22 17:06
+ **/
+
+public class TemplatemodeController {
+}
